020eb1e05d90f9489a74024b729e0e9189222662
Jeff Hollister <jeff.w.hollister@gmail.com>
2014-03-13 16:30:34 -0400

    min samp thing is getting bogus...

--- R/cpaMinSamp.R
+++ R/cpaMinSamp.R
@@ -3,15 +3,12 @@
 #' This function takes a brute force approach to estimating the minimum sample
 #' size for calculating a probability given a binary vector of data.  This is used
 #' to set the upper bounds on a conditional probability analysis.  In this 
-#' function the original vector is sampled with replacement, the probability is
-#' calculated and stored.  This is done R number of times and this distrubution of
-#' proabilities is saved
-#' 
-#' Starting with a sample size of a given minimum value, the original data is 
-#' bootstrapped for that sample size, the probability is calculated and saved.  The
-#' distribution of probabilities is tested against the original distribution to check
-#' for differences.  The smalles sample size that returns a similar distribution is the
-#' minimum sample size
+#' function the original vector is sampled with replacement starting with the minimum
+#' sample size.  A t-test is done between the original data and the sampled data.  This is
+#' repeated R number of iterations.  The stored p-values of these iterations are compared to the
+#' alpha.  The sample size where any of the p-values are less than alpha is the returned
+#' minimum sample size.  This is a very conservative estimate for the minimum sample size.
+#' Oh and this function is a complete hack job.  Not sure it should be used... 
 #' 
 #' @return an interger value indicating the minimum sample size.
 #'
@@ -26,29 +23,21 @@
 #' bivec<-rbinom(100,1,0.5)
 #' cpaMinSamp(bivec,alpha=0.01)
 #' data(binCutoff)
-#' cpaMinSamp(binCutoff,alpha=0.001,n=10)
+#' cpaMinSamp(binCutoff,n=200,R=1000,alpha=0.001)
 cpaMinSamp<-function(x,R=100,n=5,alpha=0.05){
-  
   orig<-vector("numeric",length=R)
   for(i in 1:R){
-    orig[i]<-sum(sample(x,length(x),TRUE),na.rm=TRUE)/length(x)
+    orig[i]<-sum(sample(orig,length(x),TRUE))/length(x)
   }
   test<-TRUE
   while(test){
     samp<-vector("numeric",length=R)
     for(i in 1:R){
-      samp[i]<-sum(sample(x,n,TRUE),na.rm=TRUE)/n
+      samp[i]<-sum(sample(x,n,TRUE))/n
     }
-    pv<-suppressWarnings(ks.test(orig,samp,exact=TRUE)$p.value)
-    test<-pv<alpha
-    n<-n+1
-    if(pv==0){
-      return(warning("Ties detected, try a larger minimum sample size"))
-    }
-    if(n==length(x)){
-      return(warning("all tested sample sizes resulted in different
-                      distributions.  Try reducing alpha (e.g. 0.01)"))
-    }
+    test<-sum(samp<alpha)<5
+    n<-n-1
+    print(paste(n,alpha,mean(samp)))
   }
   return(n)
 }
