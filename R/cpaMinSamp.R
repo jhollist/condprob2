cb4552d7a6a9c06bc7e6dc4d58a39f24b5f2739f
Jeff Hollister <jeff.w.hollister@gmail.com>
2014-03-14 11:16:47 -0400

    completed condprobmin and default plot for condprobmin

--- R/cpaMinSamp.R
+++ R/cpaMinSamp.R
@@ -3,41 +3,46 @@
 #' This function takes a brute force approach to estimating the minimum sample
 #' size for calculating a probability given a binary vector of data.  This is used
 #' to set the upper bounds on a conditional probability analysis.  In this 
-#' function the original vector is sampled with replacement starting with the minimum
-#' sample size.  A t-test is done between the original data and the sampled data.  This is
-#' repeated R number of iterations.  The stored p-values of these iterations are compared to the
-#' alpha.  The sample size where any of the p-values are less than alpha is the returned
-#' minimum sample size.  This is a very conservative estimate for the minimum sample size.
-#' Oh and this function is a complete hack job.  Not sure it should be used... 
+#' function the original vector is sampled with replacement at the full sample size.  
+#' This is repeated R times and mean value is recorded for each iteration. Then starting 
+#' with the minimum sample size, the original dataset is sampled and the mean for the smaller
+#' sample is recorded.  The sample size is increased and process repeated.  The average of the 
+#' mean values for each sample size is recorded  
 #' 
-#' @return an interger value indicating the minimum sample size.
+#' @return an list of class condprobMin is returned with two items.  A data frame containg
+#'          the sample size and the associated average mean value and a vector contain the
+#'          bootstrapped original mean values.
 #'
 #' @param x a vector of values to find minimum sample size for.  The values
 #'        should be 1 and 0.
 #' @param R the number of bootstrap iterations. Defaults to 100.
-#' @param n the minimum sample size to start testing from.  Defaults to 5.
-#' @param alpha the alpha level for determining confidence intervals.  Default
-#'        is 0.05 and this return the quantiles at 0.025 and 0.975.
+#' @param n the minimum sample size to start testing from.  Defaults to length of x.
+
 #' @export
 #' @examples
 #' bivec<-rbinom(100,1,0.5)
 #' cpaMinSamp(bivec,alpha=0.01)
 #' data(binCutoff)
-#' cpaMinSamp(binCutoff,n=200,R=1000,alpha=0.001)
-cpaMinSamp<-function(x,R=100,n=5,alpha=0.05){
+#' minSampDF<-cpaMinSamp(binCutoff,R=10)
+cpaMinSamp<-function(x,R=100,n=length(x)){
+  xdf<-data.frame()
   orig<-vector("numeric",length=R)
   for(i in 1:R){
-    orig[i]<-sum(sample(orig,length(x),TRUE))/length(x)
+    xo<-sample(x,length(x),TRUE)
+    orig[i]<-mean(xo)
   }
-  test<-TRUE
-  while(test){
-    samp<-vector("numeric",length=R)
-    for(i in 1:R){
-      samp[i]<-sum(sample(x,n,TRUE))/n
+  for(i in 1:n){
+    samp<-data.frame()
+    for(j in 1:R){
+      xs<-sample(x,i,TRUE)
+      samp<-rbind(samp,data.frame(i,mean(xs)))
     }
-    test<-sum(samp<alpha)<5
-    n<-n-1
-    print(paste(n,alpha,mean(samp)))
+    xdf<-rbind(xdf,data.frame(n=mean(samp[,1]),avgMean=mean(samp[,2],na.rm=T)))
+    if(i%%100==0){
+      print(paste(i,"out of",n, "completed."))
+    }
   }
-  return(n)
+  xdf<-list(sampMeans=xdf,origMeans=orig)
+  class(xdf)<-"condprobMin"
+  return(xdf)
 }
