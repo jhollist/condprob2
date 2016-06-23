3e22de9313c09e0551892254ba76da526df48176
Jeff Hollister <jeff.w.hollister@gmail.com>
2016-06-23 15:42:31 -0400

    added s3 plot method

--- R/plot.condprob.R
+++ R/plot.condprob.R
@@ -6,7 +6,7 @@
 #' data, the impaired sites, and the unimpaired sites, and a conditional probability 
 #' plot with the conditional probability on the y-axis the stressor on the x-axis.  
 #' 
-#' @param condprobObj An object of class condprob is required.  Data and parameters
+#' @param cpobj An object of class condprob is required.  Data and parameters
 #'                    from this object are used to create the plot
 #' @param ... Used primarily to pass other ggplot2 plotting options to control title, 
 #'            axis labels, colors, size, etc.
@@ -16,6 +16,10 @@
 #'             Analysis. Journal of Environmental Quality. 37(6):2392-2396.
 #'             \href{http://dx.doi.org/10.2134/jeq2007.0536}{Link}
 #' @export
-plot.condprob <- function(condprobObj, ...) {
-    
+#' @method plot condprob
+plot.condprob <- function(cpobj, ...) {
+    plot(cpobj[[1]],cpobj[[2]],xlab = names(cpobj)[1] ,ylab = names(cpobj)[2],
+         ...)
+    lines(cpobj[[1]],cpobj[[4]])
+    lines(cpobj[[1]],cpobj[[5]])
 } 
